c087c9f5caae1204965dd1bd8f4b24e7574c05a3
VadimKasyanenko <113146148+VadimKasyanenko@users.noreply.github.com>
2023-02-27 21:40:37 +0000

    Code written

---
+++ .ccls-cache/@home@runner@lab3-task1-gr13-Kasyanenko/src@interface.h
@@ -0,0 +1,5 @@
+//
+// Created by Vadim Kasyanenko 
+//
+
+#ifndef InterfaceH
--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-/* main.c */
-#include <stdio.h>
+//
+// Created by Vadim Kasyanenko 
+//
 
-void main (void)
+//check all numbers to find lucky ones
+
+#include "interface.h"
+#include "stdio.h"
+
+int main()
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	int counter=0;
+	for (int i = 100000; i < 1000000; i++) {
+		if (lucky(i)==1) {
+			printf("%d\n", i);
+			counter++;
+		}
+	}
+	printf("%d\n", counter);
 }
+
